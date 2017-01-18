d2b23ea551f4576312687b1bf2dbe269808440b0
Philipp <p.ma@live.de>
2017-01-18 09:35:04 +0100

    table create

--- MI-Medienpool_DDL.sql
+++ MI-Medienpool_DDL.sql
@@ -18,3 +18,37 @@
 
 -- Erstellen der Tabellen, Constraints und Trigger
 
+create table geraete (
+geraete_id integer generated always as identity primary key,
+geraete_name varchar2(20) not null,
+beschreibung varchar2(150) default 'Keine Beschreibung verfügbar' not null,
+anzahl int(3) not null,
+);
+
+create index index_geraete on geraete(geraete_name);
+
+create table benutzer(
+benutzer_matrkelnummer int(5) primary key, -- to do
+benutzer_name varchar2(60)),
+geraete_geliehen foreign key, -- to do
+gesperrt varchar2(4) default 'Nein' not null,
+);
+
+create table geraete_status(
+status varchar(20) default 'Verfügbar',
+datumm_von date,
+datum_bis date
+);
+
+create table reservierung (
+-- to do
+);
+
+create table verstoeße(
+-- to do
+);
+
+create geraete_verliehen (
+geraete_id integer preferences geraete(geraete_id) not null,
+
+);
