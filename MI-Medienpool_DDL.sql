ce9cdca1c5228b67a4a66b99c7df3319ec8a9c75
Philipp <p.ma@live.de>
2017-01-25 13:59:35 +0100

    Added tables etc

--- MI-Medienpool_DDL.sql
+++ MI-Medienpool_DDL.sql
@@ -1,54 +1,111 @@
--- ###################################################################
--- #                     Vorlesung Datenbanken 1                     #
--- #                                                                 #
--- # Projekt MI-Medienpool DDL Skript                                #
--- #                                                                 #
--- # Mitglieder:                                                     #
--- # Philipp Hackh, Andreas Wark,                                    #
--- # Karim Abdel Ghany                                               #                                                         
--- #                                                                 #
--- #                                                                 #
--- ###################################################################
+-- Vorlesung Datenbanken 1                 
+-- Projekt MI-Medienpool DDL Skript
+-- Teilnehmer: Philipp Hackh (ph057), Andreas Wark (aw131), Karim Abdel Ghany (ka026)                                                      
 
--- Loeschung aller Tabellen fuer einen reibungslosen Restart
--- drop
+-- DDL SQL
+-- ***********************************************************************************
 
+-- Löschen aller Constraints
+
+--Primary Key's
+alter table geraete drop constraint geraete_id_pk;
+alter table benutzer drop constraint benutzer_matrnr_pk;
+alter table geraete_verliehen drop constraint verleih_id_pk;
 
 
+--Foreing Key's
+alter table benutzer drop constraint geraete_id_fk;                  -- ?
+alter table geraete_verliehen drop constraint benutzer_matrnr_fk;
+alter table geraete_status drop constraint geraete_id_fk;            -- ?
 
--- Erstellen der Tabellen, Constraints und Trigger
+--Check 
+alter table benutzer drop constraint benutzer_gesperrt_check;
+
+-- Löschen aller Tabellen für den Restart damit keine Komplikationen auftreten
+drop table geraete;
+drop table benutzer;
+drop table geraete_verliehen;
+drop table reservierung;
+drop table verstoeße;
+drop table geraete_verliehen;
+
+-- Erstellung aller Tabellen, Indizes, Trigger und Contraints
 
 create table geraete (
-geraete_id integer generated always as identity primary key,
-geraete_name varchar2(20) not null,
-beschreibung varchar2(150) default 'Keine Beschreibung verfügbar' not null,
-anzahl int(3) not null,
+geraete_id integer generated always as identity,
+geraete_name varchar2(60) not null,
+beschreibung varchar2(300) default 'Keine Beschreibung verfügbar' not null,
+zubehoer varchar2(300) default 'Kein Zubehör verfügbar' not null,
+status varchar2(30) default 'Verfügbar' not null
 );
 
-create index index_geraete on geraete(geraete_name);
 
-create table benutzer(
-benutzer_matrkelnummer int(5) primary key, -- to do
-benutzer_name varchar2(60)),
-geraete_geliehen foreign key, -- to do
-gesperrt varchar2(4) default 'Nein' not null,
+
+create table benutzer (
+benutzer_MatrNr integer,
+benutzer_name varchar2(60),
+gesperrt varchar2(4) default 'Nein',
+geraete_geliehen varchar2(100)
 );
+
+
+create table geraete_verliehen (
+verleih_id INTEGER GENERATED ALWAYS AS IDENTITY,
+benutzer_MatrNr INTEGER REFERENCES benutzer (benutzer_MatrNr) not null,
+geraete_id INTEGER REFERENCES geraete (geraete_id) not null
+);
+
+
+
 
 create table geraete_status(
-status varchar(20) default 'Verfügbar',
-datumm_von date,
+status varchar2(20) default 'Verfügbar',
+datum_von date,
 datum_bis date
+geraete_id INTEGER REFERENCES geraete(geraete_id) not null
 );
 
+
 create table reservierung (
--- to do
+reserviert_von date,
+reserviert_bis date,
+geraete_id INTEGER REFERENCES gereate(gereate_id) not null
 );
 
 create table verstoeße(
--- to do
+verstoeße_id integer generated always as identity primary key,
+verstoeße_beschreibung varchar2(100) not null,
+verstoeße_punkte integer,
+benutzer_MatrNr varchar2 references benutzer(Benutzer_MatrNr) not null,
+geraete
 );
+-- Erstellung der Contraints und Indizes
+-- Indizies
+create index index_geraete on geraete(geraete_name);
 
-create geraete_verliehen (
-geraete_id integer preferences geraete(geraete_id) not null,
+-- Primary Key's
+alter table geraete_verliehen add constraint verleih_id_pk primary key (verleih_id)
+alter table benutzer add constraint benutzer_matrnr_pk primary key (benutzer_matrnr);
+alter table geraete add constraint geraete_id_pk primary key (geraete_id);
 
-);
+
+-- Foreign Key's
+alter table geraete_status add constraint geraete_id_fk foreign key (geraete) references geraete (geraete_id);
+alter table geraete_verliehen add constraint benutzer_matrnr_fk foreign key (geraete_verliehen) references benutzer(benutzer_matrnr);
+alter table benutzer add constraint geraete_id_fk foreign key (geraete) references geraete (geraete_id);
+
+
+-- Check's
+ALTER TABLE benutzer ADD CONSTRAINT benutzer_gesperrt_check CHECK (gesperrt IN('Ja', 'Nein'));
+
+-- trigger
+
+CREATE OR REPLACE TRIGGER verstoeße_trigger
+  AFTER INSERT ON verstoeße
+  referencing new as new old as old for each row
+BEGIN
+  when new.verstoeße_punkte >= 10 then
+  INSERT INTO benutzer (gesperrt) VALUES ('JA')
+
+END
+/
--- MI-Medienpool_DML.sql
+++ MI-Medienpool_DML.sql
@@ -1,13 +1,43 @@
--- ###################################################################
--- #                     Vorlesung Datenbanken 1                     #
--- #                                                                 #
--- # Projekt MI-Medienpool DDL Skript                                #
--- #                                                                 #
--- # Mitglieder:                                                     #
--- # Philipp Hackh, Andreas Wark,                                    #
--- # Karim Abdel Ghany                                               #                                                         
--- #                                                                 #
--- #                                                                 #
--- ###################################################################
+-- Vorlesung Datenbanken 1                 
+-- Projekt MI-Medienpool DDL Skript
+-- Teilnehmer: Philipp Hackh (ph057), Andreas Wark (aw131), Karim Abdel Ghany (ka026)                                              #                                                         
 
--- Insert-Skript
+-- DML SQL
+-- ***********************************************************************************
+
+-- Geraete Insert's
+INSERT INTO geraete (geraete_name, beschreibung, zubehoer) 
+VALUES ('MacBook Pro', 'Intel m5 1,2 GHz Dual Core, 8GB Ram, Intel HD Graphics 515, 512 GB Festplatte', 'Ladekabel');
+INSERT INTO geraete (geraete_name, zubehoer)
+VALUES ('Apple Watch Generation 2', 'Ladekabel');
+INSERT INTO geraete (geraete_name, zubehoer)
+VALUES ('Oculus Rift', 'Zwei Controller, HDMI-Verbindungskabel, Motiontracking-Kamera, Stromkabel');
+
+-- Benutzer Insert's
+INSERT INTO benutzer (benutzer_MatrNr, benutzer_name)
+VALUES (31104, 'Hackh, Philipp');
+INSERT INTO benutzer (benutzer_MatrNr, benutzer_name)
+VALUES (31094, 'Abdel Ghany, Karim ');
+INSERT INTO benutzer (benutzer_MatrNr, benutzer_name)
+VALUES (32051, 'Wark, Andreas');
+
+-- Geraete_verliehen Insert's
+INSERT INTO geraete_verliehen (benutzer_MatrNr, geraete_id)
+VALUES (31104, 2);
+INSERT INTO geraete_verliehen (benutzer_MatrNr, geraete_id)
+VALUES (32051, 1);
+INSERT INTO gereate_verliehen (benutzer_MatrNr, geraete_id)
+VALUES (32051, 3);
+
+-- verstoeﬂe Insert's
+INSERT INTO verstoeﬂe (verstoeﬂe_beschreibung, verstoeﬂe_punkte, benutzer_MatrNr)
+VALUES ('Leihdauer ¸berzogen, Ger‰t noch nicht zur¸ckgebracht', 10, 31104);
+INSERT INTO verstoeﬂe (verstoeﬂe_beschreibung, verstoeﬂe_punkte, benutzer_MatrNr)
+VALUES ('Displaykratzer', 4, 31094);
+INSERT INTO verstoeﬂe (verstoeﬂe_beschreibung, verstoeﬂe_punkte, benutzer_MatrNr)
+VALUES ('to doo', 6, 31094)
+
+
+select * from geraete;
+select * from benutzer;
+select * from geraete_verliehen;
