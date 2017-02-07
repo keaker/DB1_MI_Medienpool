742c633e3684646c5ee45ef3da96cf8d42e6d623
Philipp <p.ma@live.de>
2017-02-07 17:08:18 +0100

    Table Working, Inserts

--- MI-Medienpool_DDL.sql
+++ MI-Medienpool_DDL.sql
@@ -4,51 +4,65 @@
 
 -- DDL SQL
 -- ***********************************************************************************
-alter table geraete drop constraint geraete_id_index;
+
 -- Löschen aller Constraints
 -- Foreing Key's
-alter table geraete_verliehen drop constraint benutzer_matrnr_fk;
+ALTER table geraete_verliehen drop constraint benutzer_matrnr_fk;
 alter table geraete_verliehen drop constraint geraete_verliehen_id_fk;
 alter table geraete_status drop constraint geraete_status_id_fk;
-alter table reservierung drop constraint reservierung_geraete_id_fk;
-alter table verstoeße drop constraint verstoeße_benutzer_matrnr_fk;
-alter table verstoeße drop constraint verstoßart_id_fk;
+Alter table reservierung drop constraint reservierung_geraete_id_fk;
+alter table verstoesse drop constraint verstoesse_benutzer_matrnr_fk;
+alter table verstoesse drop constraint verstossart_id_fk;
+alter table geraete drop constraint geraeteart_id_fk;
+alter table reservierung drop constraint reservierung_matrnr_fk;
+
+
+
+-- Check 
+alter table benutzer drop constraint benutzer_gesperrt_check;
+Alter table geraete_status drop constraint status_check;
+alter table geraetearten drop constraint geraeteart_id_check;
+alter table geraetearten drop constraint geraeteart_check;
+alter table verstossarten drop constraint verstossarten_id_check;
+alter table verstossarten drop constraint verstossart_beschreibung_check;
 
 -- Primary Key's
 alter table benutzer drop constraint benutzer_matrnr_pk;
 alter table geraete_verliehen drop constraint verleih_id_pk;
+alter table geraetearten drop constraint geraeteart_id_pk;
+alter table verstossarten drop constraint verstossarten_id_pk;
 alter table geraete drop constraint geraete_id_pk;
 
--- Check 
-
-alter table benutzer drop constraint benutzer_gesperrt_check;
-alter table geraete_status drop constraint status_check;
--- Indizies
-
-
- 
+-- Trigger
+drop trigger trg_verstoesse_insert_1;
 
 -- Löschen aller Tabellen für den Restart damit keine Komplikationen auftreten
+drop table geraetearten;
 drop table geraete;
 drop table benutzer;
 drop table geraete_verliehen;
 drop table geraete_status;
 drop table reservierung;
-drop table verstoeße;
+drop table verstossarten;
+drop table verstoesse;
 
 -- Erstellung aller Tabellen, Indizes, Trigger und Contraints
 create table geraetearten (
-geraete_id integer GENERATED ALWAYS AS IDENTITY,
-geraete_name varchar2(60) not null,
+geraeteart_id INTEGER not null,
+geraeteart varchar(60) not null
+);
+
+create table geraete (
+geraete_id integer generated always as identity,
+geraeteart_id integer not null,
 beschreibung varchar2(300) default 'Keine Beschreibung verfügbar' not null,
-zubehoer varchar2(300) default 'Kein Zubehör verfügbar' not null,
-status varchar2(30) default 'Verfügbar' not null
+zubehoer varchar2(300) default 'Kein Zubehör verfügbar' not null
 );
 
 create table benutzer (
 benutzer_matrnr integer,
 benutzer_name varchar2(60),
-benutzer_verstoßpunkte integer default 0,
+benutzer_verstosspunkte integer default 0,
 gesperrt varchar2(4) default 'Nein'
 );
 
@@ -62,62 +76,80 @@ create table geraete_status(
 status varchar2(20) default 'Verfügbar',
 datum_von date,
 datum_bis date,
-geraete_id integer not null --FK
+geraete_id integer not null
 );
 
 create table reservierung (
-reserviert_von date,
-reserviert_bis date,
+reservierung_id integer generated always as identity,
+benutzer_matrnr integer not null,
+reserviert_von date not null,
+reserviert_bis date not null,
 geraete_id INTEGER not null
 );
 
-create table verstoßarten(
-verstoßart_id integer generated always as identity,
-verstoßart_beschreibung varchar2(100),
-verstoßart_punkte integer not null
+create table verstossarten(
+verstossart_id integer, 
+verstossart_beschreibung varchar2(100),
+verstossart_punkte integer not null
 );
 
-create table verstoeße(
-verstoeße_id integer generated always as identity,
-verstoßart_id integer,
-benutzer_MatrNr varchar2(5) not null
+create table verstoesse(
+verstoesse_id integer generated always as identity,
+verstossart_id integer,
+benutzer_matrnr integer not null
 );
 
 
 -- Erstellung der Contraints und Indizes
 -- Indizies
--- create index index_geraete on geraete(geraete_id); ??????
+create index geraete_id_index on geraete(geraete_id);
 
 -- Check's
 ALTER TABLE benutzer ADD CONSTRAINT benutzer_gesperrt_check CHECK (gesperrt IN('Ja', 'Nein'));
 ALTER TABLE geraete_status ADD CONSTRAINT status_check CHECK (status IN ('Verfügbar','Ausgeliehen','In Reperatur','Reserviert'));
+ALTER TABLE geraetearten ADD CONSTRAINT geraeteart_id_check CHECK (geraeteart_id IN (1, 2, 3, 4, 5));
+ALTER TABLE geraetearten ADD CONSTRAINT geraeteart_check CHECK  (geraeteart in ('Laptop', 'Wearable', 'Smartphone', 'Tablet', 'Sonstiges'));
+alter table verstossarten add constraint verstossarten_id_check check (verstossart_id in (1 ,2 ,3));
+alter table verstossarten add constraint verstossart_beschreibung_check check (verstossart_beschreibung in ('Leihdauer ueberzogen', 'Geraet defekt zurueckgebracht', 'Geraet verkratzt/beschaedigt'));
 
 -- Primary Key's
 alter table benutzer add constraint benutzer_matrnr_pk primary key (benutzer_matrnr);
 alter table geraete_verliehen add constraint verleih_id_pk primary key (verleih_id);
+alter table geraetearten add constraint geraeteart_id_pk primary key (geraeteart_id);
+alter table verstossarten add constraint verstossarten_id_pk primary key (verstossart_id);
 alter table geraete add constraint geraete_id_pk primary key (geraete_id);
-
 
 -- Foreign Key's
 alter table geraete_verliehen add constraint benutzer_matrnr_fk foreign key (benutzer_MatrNr) references benutzer(benutzer_matrnr);
-alter table geraete_verliehen add constraint geraete_verliehen_id_fk foreign key (geraete_id) references geraete(geraete_id);
+alter table geraete_verliehen add constraint geraete_verliehen_id_fk foreign key (geraete_id) references geraetearten(geraeteart_id);
 alter table geraete_status add constraint geraete_status_id_fk foreign key (geraete_id) references geraete (geraete_id);
-alter table reservierung add constraint reservierung_geraete_id_fk foreign key (geraete_id) references geraete (geraete_id);
-alter table verstoeße add constraint verstoeße_benutzer_matrnr_fk foreign key (benutzer_matrnr) references benutzer(benutzer_matrnr);
-alter table verstoeße add constraint verstoßart_id_fk foreign key (verstoßart_id) references verstoßarten(verstoßart_id);
+alter table reservierung add constraint reservierung_geraete_id_fk foreign key (geraete_id) references geraete (geraete_id); -- ?
+alter table verstoesse add constraint verstoesse_benutzer_matrnr_fk foreign key (benutzer_matrnr) references benutzer(benutzer_matrnr);
+alter table geraete add constraint geraeteart_id_fk foreign key (geraeteart_id) references geraetearten(geraeteart_id);
+alter table verstoesse add constraint verstossart_id_fk foreign key (verstossart_id) references verstossarten(verstossart_id);
+alter table reservierung add constraint reservierung_matrnr_fk foreign key (benutzer_matrnr) references benutzer(benutzer_matrnr);
 
 
-create index geraete_id_index on geraete (geraete_id);
-
--- trigger
-/*
-CREATE OR REPLACE TRIGGER verstoeße_trigger
-  AFTER INSERT ON verstoeße
-  referencing new as new old as old for each row
+-- trigger testversuch
+CREATE OR REPLACE TRIGGER trg_verstoesse_insert_1
+  AFTER INSERT ON verstoesse
+  REFERENCING NEW AS NEW OLD AS OLD
+  FOR EACH ROW
+DECLARE
+  b_verstosspunkte integer;
+  b_matrnr integer;
+  v_verstosspunkte integer;
 BEGIN
-  when new.verstoeße_punkte >= 10 then
-  INSERT INTO benutzer (gesperrt) VALUES ('Ja')
-
-END
+  SELECT benutzer_verstosspunkte, benutzer_matrnr
+  INTO b_verstosspunkte, b_matrnr
+  FROM benutzer
+  WHERE benutzer_matrnr = :NEW.benutzer_matrnr;
+  SELECT verstossart_verstosspunkte
+  INTO v_verstosspunkte
+  from verstossarten
+  WHERE verstossart_id = :NEW.verstossart_id;
+  b.verstosspunkte := b_verstosspunkte + v_verstosspunkte;
+  --WHERE verstoesse.benutzer_matrnr = benutzer.benutzer_matrnr AND verstoesse.verstossart_id = verstossarten.verstossart_id;
+END;
 /
-*/
+
--- MI-Medienpool_DML.sql
+++ MI-Medienpool_DML.sql
@@ -4,41 +4,80 @@
 
 -- DML SQL
 -- ***********************************************************************************
+-- Geraetearten Insert's
+INSERT INTO geraetearten (geraeteart_id, geraeteart) 
+VALUES (1, 'Laptop');
+INSERT INTO geraetearten (geraeteart_id, geraeteart) 
+VALUES (2, 'Wearable');
+INSERT INTO geraetearten (geraeteart_id, geraeteart) 
+VALUES (3, 'Smartphone');
+INSERT INTO geraetearten (geraeteart_id, geraeteart) 
+VALUES (4, 'Tablet');
+INSERT INTO geraetearten (geraeteart_id, geraeteart) 
+VALUES (5, 'Sonstiges');
 
--- Geraete Insert's
-INSERT INTO geraete (geraete_name, beschreibung, zubehoer) 
-VALUES ('MacBook Pro', 'Intel m5 1,2 GHz Dual Core, 8GB Ram, Intel HD Graphics 515, 512 GB Festplatte', 'Ladekabel');
-INSERT INTO geraete (geraete_name, zubehoer)
-VALUES ('Apple Watch Generation 2', 'Ladekabel');
-INSERT INTO geraete (geraete_name, zubehoer)
-VALUES ('Oculus Rift', 'Zwei Controller, HDMI-Verbindungskabel, Motiontracking-Kamera, Stromkabel');
+-- Gereate Insert's
+INSERT INTO geraete (geraeteart_id, beschreibung, zubehoer)
+VALUES (3, 'iPhone 7 128GB Spacegrau', 'Ladekabel, EarPods');
+INSERT INTO geraete (geraeteart_id, beschreibung, zubehoer)
+VALUES (2, 'Apple Watch 2 Spacegrau', 'Ladekabel');
+INSERT INTO geraete (geraeteart_id, beschreibung, zubehoer)
+VALUES (4, 'iPad Air 2 64GB silber', 'Ladekabel');
+INSERT INTO geraete (geraeteart_id, beschreibung, zubehoer)
+VALUES (1, 'MacBook Pro 2017 Spacegrau mit TouchBar', 'Ladekabel');
+INSERT INTO geraete (geraeteart_id, beschreibung, zubehoer)
+VALUES (5, 'HP Photosmart 8850 Drucker', 'Stromkabel');
+INSERT INTO geraete (geraeteart_id, beschreibung, zubehoer)
+VALUES (3, 'Samsung Galaxy S7', 'Ladekabel, Kopfhoerer');
 
 -- Benutzer Insert's
-INSERT INTO benutzer (benutzer_MatrNr, benutzer_name)
-VALUES (31104, 'Hackh, Philipp');
-INSERT INTO benutzer (benutzer_MatrNr, benutzer_name)
-VALUES (31094, 'Abdel Ghany, Karim ');
-INSERT INTO benutzer (benutzer_MatrNr, benutzer_name)
-VALUES (32051, 'Wark, Andreas');
+INSERT INTO benutzer (benutzer_matrnr, benutzer_name, benutzer_verstosspunkte)
+VALUES (31104, 'Hackh, Philipp', 0);
+INSERT INTO benutzer (benutzer_matrnr, benutzer_name, benutzer_verstosspunkte, gesperrt)
+VALUES (31094, 'Abdel Ghany, Karim ', 10, 'Ja');
+INSERT INTO benutzer (benutzer_matrnr, benutzer_name, benutzer_verstosspunkte)
+VALUES (32051, 'Wark, Andreas', 4);
 
 -- Geraete_verliehen Insert's
-INSERT INTO geraete_verliehen (benutzer_MatrNr, geraete_id)
+INSERT INTO geraete_verliehen (benutzer_matrnr, geraete_id)
 VALUES (31104, 2);
 INSERT INTO geraete_verliehen (benutzer_MatrNr, geraete_id)
 VALUES (32051, 1);
-INSERT INTO gereate_verliehen (benutzer_MatrNr, geraete_id)
+INSERT INTO geraete_verliehen (benutzer_MatrNr, geraete_id)
 VALUES (32051, 3);
 
 -- Geraete_status Insert's
 INSERT INTO geraete_status (status, datum_von, datum_bis, geraete_id)
 VALUES ('Ausgeliehen', TO_DATE('05.02.2017', 'DD.MM.YYYY'), TO_DATE('25.02.2017', 'DD.MM.YYYY'), 1);
+INSERT INTO geraete_status (status, datum_von, datum_bis, geraete_id)
+VALUES ('In Reperatur', TO_DATE('01.02.2017', 'DD.MM.YYYY'), TO_DATE('28.02.2017', 'DD.MM.YYYY'), 6);
+INSERT INTO geraete_status (status, datum_von, datum_bis, geraete_id)
+VALUES ('Ausgeliehen', TO_DATE('08.02.2017', 'DD.MM.YYYY'), TO_DATE('16.02.2017', 'DD.MM.YYYY'), 2);
+INSERT INTO geraete_status (status, datum_von, datum_bis, geraete_id)
+VALUES ('Ausgeliehen', TO_DATE('07.02.2017', 'DD.MM.YYYY'), TO_DATE('09.02.2017', 'DD.MM.YYYY'), 3);
 
+-- Reservierung Insert's
+INSERT INTO reservierung (benutzer_matrnr, reserviert_von, reserviert_bis, geraete_id)
+VALUES (31104, TO_DATE('07.02.2017', 'DD.MM.YYYY'), TO_DATE('10.02.2017', 'DD.MM.YYYY'), 4);
+INSERT INTO reservierung (benutzer_matrnr, reserviert_von, reserviert_bis, geraete_id)
+VALUES (32051, TO_DATE('09.02.2017', 'DD.MM.YYYY'), TO_DATE('15.02.2017', 'DD.MM.YYYY'), 5);
+--INSERT INTO reservierung (reservierung_id, benutzer_matrnr, reserviert_von, reserviert_bis, geraete_id)
+--VALUES (3, 31104, TO_DATE('04.02.2017', 'DD.MM.YYYY'), TO_DATE('02.03.2017', 'DD.MM.YYYY'), 3);
 
+-- verstossarten Insert's
+INSERT INTO verstossarten (verstossart_id, verstossart_beschreibung, verstossart_punkte)
+VALUES (1, 'Leihdauer ueberzogen', 2);
+INSERT INTO verstossarten (verstossart_id, verstossart_beschreibung, verstossart_punkte)
+VALUES (2, 'Geraet defekt zurueckgebracht', 5);
+INSERT INTO verstossarten (verstossart_id, verstossart_beschreibung, verstossart_punkte)
+VALUES (3, 'Geraet verkratzt/beschaedigt', 1);
 
--- verstoeﬂe Insert's
-INSERT INTO verstoeﬂe (verstoeﬂe_beschreibung, verstoeﬂe_punkte, benutzer_MatrNr)
-VALUES ('Leihdauer ¸berzogen, Ger‰t noch nicht zur¸ckgebracht', 10, 31104);
-INSERT INTO verstoeﬂe (verstoeﬂe_beschreibung, verstoeﬂe_punkte, benutzer_MatrNr)
-VALUES ('Displaykratzer', 4, 31094);
-INSERT INTO verstoeﬂe (verstoeﬂe_beschreibung, verstoeﬂe_punkte, benutzer_MatrNr)
-VALUES ('to doo', 6, 31094);
+/*-- TO DOO !!!!
+-- verstoesse Insert's
+INSERT INTO verstoesse (verstoesse_id, verstossart_id, benutzer_matrnr)
+VALUES (1, 1, 32051);
+INSERT INTO verstoesse (verstoesse_id, verstossart_id, benutzer_matrnr)
+VALUES (2, 2, 32051);
+INSERT INTO verstoesse (verstoesse_id, verstossart_id, benutzer_matrnr)
+VALUES (3, 1, 31104);
+*/
